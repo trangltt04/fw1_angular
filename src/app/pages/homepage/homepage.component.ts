abbd239a7dae0133755789ff3dd66208ca34d17f
trangltt04 <tranglttph45373@fpt.edu.vn>
2024-09-13 17:24:11 +0700

    day 2: add for item

--- src/app/pages/homepage/homepage.component.ts
+++ src/app/pages/homepage/homepage.component.ts
@@ -1,11 +1,54 @@
+import { NgFor } from '@angular/common';
 import { Component } from '@angular/core';
-import { HeaderComponent } from '../../component/header/header.component';
+
+type Product = {
+  id: number;
+  title: string;
+  image: string;
+  price: number;
+  description: string;
+};
 
 @Component({
   selector: 'app-homepage',
   standalone: true,
-  imports: [HeaderComponent],
+  imports: [NgFor],
   templateUrl: './homepage.component.html',
   styleUrl: './homepage.component.css',
 })
-export class HomepageComponent {}
+export class HomepageComponent {
+  products: Product[] = [
+    {
+      id: 1,
+      title: 'San pham a',
+      description: 'Mo ta san pham a',
+      price: 111,
+      image:
+        'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ2RWQ0h1LZxZHLad9eB0JVgBlzqEPPrZVeQQ&s',
+    },
+    {
+      id: 2,
+      title: 'San pham b',
+      description: 'Mo ta san pham b',
+      price: 222,
+      image:
+        'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ2RWQ0h1LZxZHLad9eB0JVgBlzqEPPrZVeQQ&s',
+    },
+    {
+      id: 3,
+      title: 'San pham c',
+      description: 'Mo ta san pham c',
+      price: 333,
+      image:
+        'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ2RWQ0h1LZxZHLad9eB0JVgBlzqEPPrZVeQQ&s',
+    },
+    {
+      id: 4,
+      title: 'San pham d',
+      description: 'Mo ta san pham d',
+      price: 444,
+      image:
+        'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ2RWQ0h1LZxZHLad9eB0JVgBlzqEPPrZVeQQ&s',
+    },
+  ];
+}
