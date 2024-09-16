433cded987cecfc9022c1cba9813fb6d95903160
trangltt04 <tranglttph45373@fpt.edu.vn>
2024-09-16 09:32:20 +0700

    service

---
+++ src/app/services/product.service.ts
@@ -0,0 +1,19 @@
+import { HttpClient } from '@angular/common/http';
+import { inject, Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class ProductService {
+  // get All
+  // getDetail
+  //add
+  // edit
+  // delete
+
+  http = inject(HttpClient);
+
+  getAll() {
+    return this.http.get('http://localhost:3000/products');
+  }
+}
